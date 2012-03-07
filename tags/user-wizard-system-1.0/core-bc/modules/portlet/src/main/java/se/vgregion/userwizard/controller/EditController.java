d0d1f8759d4b0608f3b34ca3d62e975908265df1
simon.goransson@gmail.com <simon.goransson@gmail.com@22daba53-662a-730c-f077-cc9e7c0f7dd8>
2012-03-07 15:49:29 +0000

    [maven-release-plugin]  copy for tag user-wizard-system-1.0

---
+++ tags/user-wizard-system-1.0/core-bc/modules/portlet/src/main/java/se/vgregion/userwizard/controller/EditController.java
@@ -0,0 +1,117 @@
+package se.vgregion.userwizard.controller;
+
+import java.io.IOException;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+import javax.portlet.PortletException;
+import javax.portlet.PortletMode;
+import javax.portlet.PortletModeException;
+import javax.portlet.PortletPreferences;
+import javax.portlet.ReadOnlyException;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+import javax.portlet.ValidatorException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.portlet.bind.annotation.ActionMapping;
+import org.springframework.web.portlet.bind.annotation.RenderMapping;
+
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portal.theme.ThemeDisplay;
+import com.liferay.portlet.journal.service.JournalArticleLocalService;
+
+/**
+ * @author Erik Andersson
+ * 
+ */
+
+@Controller
+@RequestMapping(value = "EDIT")
+public class EditController {
+	
+	@Autowired
+    private JournalArticleLocalService journalArticleLocalService;
+	
+    @RenderMapping
+    public String doEdit(RenderRequest request, RenderResponse response) throws IOException,
+            PortletException {
+    	
+    	boolean postedWizardArticleExists = Boolean.parseBoolean(ParamUtil.getString(request, "postedWizardArticleExists", "true"));
+    	boolean isEditResponse = Boolean.parseBoolean(ParamUtil.getString(request, "isEditResponseStr", "false"));
+    	String postedWizardArticleId = ParamUtil.getString(request, "postedWizardArticleId", "");
+    	
+        PortletPreferences prefs = request.getPreferences();
+
+        request.setAttribute("isEditResponse", isEditResponse);
+        request.setAttribute("postedWizardArticleExists", postedWizardArticleExists);
+        request.setAttribute("postedWizardArticleId", postedWizardArticleId);
+        request.setAttribute("wizardArticleId", prefs.getValue("wizardArticleId", ""));
+
+        return "edit";
+    }
+
+    /*
+     * Sets article value for wizard article in portlet preferences
+     * 
+     * @comment
+     * If portlet needs to be used on multiple pages with the same setting,
+     * the implementation should be changed from portlet preferences to
+     * setting the articleId value as an expandoValue for the company.
+     * To to this, use the CommunityExpandoHelper or CompanyExpandoHelper class
+     * After such a change, update doEdit above, and UserWizardController viewWizard
+     * 
+     * 
+     * */
+    @ActionMapping(params = "action=savePref")
+    public void savePref(ActionRequest request, ActionResponse response) {
+    	
+		ThemeDisplay themeDisplay =(ThemeDisplay) request.getAttribute(WebKeys.THEME_DISPLAY);
+    	long scopeGroupId = themeDisplay.getScopeGroupId();
+
+        String wizardArticleIdStr = ParamUtil.getString(request, "wizardArticleId");
+        
+        boolean hasWizardArticle = false;
+        
+        try {
+        	hasWizardArticle = journalArticleLocalService.hasArticle(scopeGroupId, wizardArticleIdStr);
+        	
+		} catch (SystemException e) {
+			_log.error(e, e);
+		}
+
+		_log.info("EditController - savePref - hasWizardArticle:" + hasWizardArticle);
+
+		if(hasWizardArticle) {
+	        try {
+	            PortletPreferences prefs = request.getPreferences();
+	            prefs.setValue("wizardArticleId", wizardArticleIdStr);
+	            prefs.store();
+	            
+				response.setPortletMode(PortletMode.VIEW);
+	            
+	        } catch (ReadOnlyException e) {
+	            _log.error(e, e);
+	        } catch (IOException e) {
+	        	_log.error(e, e);
+	        } catch (ValidatorException e) {
+	        	_log.error(e, e);
+	        }
+			catch (PortletModeException e) {
+				_log.error(e, e);
+			}
+		} else {
+			response.setRenderParameter("isEditResponseStr", "true");
+			response.setRenderParameter("postedWizardArticleExists", "false");
+			response.setRenderParameter("postedWizardArticleId", wizardArticleIdStr);
+		}
+    }
+
+    private static Log _log = LogFactoryUtil.getLog(EditController.class);
+}
---
+++ tags/user-wizard-system-1.0/core-bc/modules/portlet/src/main/java/se/vgregion/userwizard/hook/UserWizardLoginPostAction.java
@@ -0,0 +1,118 @@
+package se.vgregion.userwizard.hook;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import se.vgregion.liferay.expando.UserExpandoHelper;
+
+import com.liferay.portal.kernel.events.Action;
+import com.liferay.portal.kernel.events.ActionException;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.model.User;
+import com.liferay.portal.service.UserLocalService;
+import com.liferay.portlet.expando.model.ExpandoTableConstants;
+import com.liferay.portlet.expando.service.ExpandoValueLocalService;
+
+public class UserWizardLoginPostAction  extends Action {
+	
+	private static Log _log = LogFactoryUtil.getLog(UserWizardLoginPostAction.class);
+	
+    private ApplicationContext applicationContext;
+    private UserLocalService userLocalService;
+    private ExpandoValueLocalService expandoValueLocalService;
+    
+    private static final String EXPANDO_VALUE_HIDE_RP_WIZARD_LOGGED_IN_SESSION = "hide_rp_wizard_logged_in_session";
+		
+    @Override
+    public void run(HttpServletRequest request, HttpServletResponse response) throws ActionException {
+        init();
+        
+        _log.info("UserWizardLoginPostAction...");
+
+        User user = lookupUser(request);
+        if (user == null) {
+        	_log.info("UserWizardLoginPostAction user is null.");
+            return;
+        }
+
+        try {
+        	resetUserWizardLoggedInSession(user);
+        } catch (Exception e) {
+            log(e.getMessage(), e);
+        } finally {
+        }
+    }
+    
+    private User lookupUser(HttpServletRequest request) {
+        try {
+            Long userId = Long.parseLong(request.getRemoteUser());
+
+            User user = userLocalService.getUser(userId);
+            if (user == null) {
+                throw new Exception("User was not found");
+            }
+            return user;
+        } catch (Exception ex) {
+            String msg = String.format("There is no user with id [%s] in Liferay's database.",
+                    request.getRemoteUser());
+            log(msg, ex);
+        }
+        return null;
+    }
+    
+    /**
+     * Sets the expando value "hide_rp_wizard_logged_in_session" to false for a Liferay user.
+     * 
+     * @param user
+     *            the Liferay user to update
+     * 
+     * @comment Should be separated into a service            
+     */
+    private void resetUserWizardLoggedInSession(User user) {
+    	long companyId = user.getCompanyId();
+    	long userId = user.getUserId();
+    	
+    	try {
+    		
+			expandoValueLocalService.addValue(companyId, User.class.getName(), ExpandoTableConstants.DEFAULT_TABLE_NAME,
+						EXPANDO_VALUE_HIDE_RP_WIZARD_LOGGED_IN_SESSION, userId, false);
+		} catch (PortalException e) {
+			_log.error(e, e);
+		} catch (SystemException e) {
+			_log.error(e, e);
+		}
+    }
+
+    private void init() {
+        if (userLocalService == null) {
+            userLocalService = (UserLocalService) getApplicationContext().getBean("userLocalService");
+        }
+        
+        if(expandoValueLocalService == null) {
+        	expandoValueLocalService = (ExpandoValueLocalService) getApplicationContext().getBean("expandoValueLocalService");
+        }
+    }
+
+    private ApplicationContext getApplicationContext() {
+        if (applicationContext == null) {
+            applicationContext = new ClassPathXmlApplicationContext("userwizard-hook-application-context.xml");
+        }
+        return applicationContext;
+    }
+
+    private void log(String msg, Throwable ex) {
+        if (_log.isDebugEnabled()) {
+        	_log.warn(msg, ex);
+        } else {
+        	_log.warn(msg);
+        }
+    }
+	
+}
